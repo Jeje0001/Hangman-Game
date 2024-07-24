0ae5644d0bd778648af6a49778af7ab27903056e
Jeje0001 <157323284+Jeje0001@users.noreply.github.com>
2024-07-24 16:09:32 -0400

    kk
    ll

---
+++ app.js
@@ -0,0 +1,9 @@
+
+const keyboard=document.querySelector(".keyboard")
+for (let index = 97; index < 122; index++) {
+    const button=document.createElement("button")
+    button.innerText=String.fromCharCode(index)
+
+}
+
+//continue at 18:00
