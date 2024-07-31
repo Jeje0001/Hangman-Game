eab3716144764eea53eb1f7bcd26749de1049a37
Jeje0001 <157323284+Jeje0001@users.noreply.github.com>
2024-07-31 13:25:20 -0400

    hh
    hh

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 
 const keyboard=document.querySelector(".keyboard")
+const guesstext=document.querySelector(".incorrect b")
 const worddisplay=document.querySelector(".worddisplay")
+const hangman=document.querySelector(".mainbox img")
 let current;
-wrongguess=0
+let wrongguess=0
 const maxguess=6
+let correct=[]
+let gamemodal= document.querySelector(".gamemodal")
 const getrandom=()=>{
     const { word , hint} = wordList[Math.floor(Math.random() * wordList.length)]
     console.log(word,hint)
@@ -11,24 +15,40 @@ const getrandom=()=>{
     document.querySelector(".hint").innerText=`Hint:${hint}`
     worddisplay.innerHTML=word.split("").map(()=>`<li class="letter"></li>`).join("")
 }
+const gameover=(victory)=>{
+    setTimeout(()=>{
+        const modaltext=victory ? `You found the word` :`The correct word was :`;
+        gamemodal.classList.add("show")
+
+    },300)
+}
 getrandom()
 const initgame=(button,clickedletter)=>{
     if (current.includes(clickedletter)){
         [...current].forEach((letter,index)=>{
             if(letter === clickedletter ){
+                correct.push(letter)
                 worddisplay.querySelectorAll("li")[index].innerText=letter;
                 worddisplay.querySelectorAll("li")[index].classList.add("letter");
             }
         })
 
     }else{
-        console.log(clickedletter,"Does not exist")
+        wrongguess+=1
+        hangman.src=`images/hangman-${wrongguess}.svg`
+    }
+    button.disabled=true;
+    guesstext.innerText=`${wrongguess} / ${maxguess}`;
 
+    if (wrongguess == maxguess){
+        return gameover(false);
+    }
+    if (correct.length == current.length){
+        return gameover(true);
     }
 
-
 }
-for (let index = 97; index < 122; index++) {
+for (let index = 97; index < 123; index++) {
     const button=document.createElement("button")
     button.innerText=String.fromCharCode(index)
     keyboard.appendChild(button)
@@ -37,5 +57,5 @@ for (let index = 97; index < 122; index++) {
 }
 
 
-// continue at 26:35
+// continue at 33:21
 
